3a4ebcf41141e133d31c117017e13258e4db6512
sinaEbrahimi <sinaebrahimi31@gmail.com>
2023-09-23 00:29:35 +0330

    Navbar  created

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className=" bg-yellow-300">{children}</body>
+      <body className=" bg-slate-100">{children}</body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,15 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main className=" text-red-600">
-      <p>helooo</p>
+    <main className="">
+      <div className="bg-gradient-to-tr from-madison-700 to-madison-500">
+        <nav className=" container mx-auto  flex flex-col items-center justify-center py-8">
+          <h1 className="mb-5 text-4xl font-semibold text-white">
+            Temp monitor
+          </h1>
+          <p className="text-madison-200">version 1</p>
+        </nav>
+      </div>
     </main>
   );
 }
